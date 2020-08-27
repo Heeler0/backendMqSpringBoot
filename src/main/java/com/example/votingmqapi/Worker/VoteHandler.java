87dac63893614b4f0f1aee1be1c1570e3b60d8af
haleit <haleit@ttu.ee>
2020-08-28 00:41:07 +0300

    Smaller changes and adding sql script to make table

--- src/main/java/com/example/votingmqapi/Worker/VoteHandler.java
+++ src/main/java/com/example/votingmqapi/Worker/VoteHandler.java
@@ -1,18 +1,23 @@
 package com.example.votingmqapi.Worker;
 
 
+import com.example.votingmqapi.Database.Votes;
+import com.example.votingmqapi.Database.VotesRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VoteHandler {
 
+    @Autowired
+    VotesRepository votesRepository;
 
 
     @RabbitListener(queues = "votesQueue")
     public void receiveMessage(String message) {
-        System.out.println("Received Message:" + message);
-        System.out.println();
+        Votes vote = new Votes(message);
+        votesRepository.save(vote);
     }
 
 }
