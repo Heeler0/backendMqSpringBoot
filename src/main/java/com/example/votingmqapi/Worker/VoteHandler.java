47292ab04cb0fc910de138977552900dba480c99
haleit <haleit@ttu.ee>
2020-08-27 21:45:10 +0300

    Worker to take messages from que

---
+++ src/main/java/com/example/votingmqapi/Worker/VoteHandler.java
@@ -0,0 +1,18 @@
+package com.example.votingmqapi.Worker;
+
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VoteHandler {
+
+    
+
+    @RabbitListener(queues = "votesQueue")
+    public void receiveMessage(String message) {
+        System.out.println("Received Message:" + message);
+        System.out.println();
+    }
+
+}
